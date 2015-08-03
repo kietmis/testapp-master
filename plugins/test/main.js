fef5e10faa7f1a9ce2992993d25c33e589e9c25b
kietmis <kietmis@gmail.com>
2015-08-03 09:45:30 +0500

    uncommnted all

--- plugins/test/main.js
+++ plugins/test/main.js
@@ -1,6 +1,6 @@
 var templates = [
-    //"root/externallib/text!root/plugins/test/theme.css",
-    //"root/externallib/text!root/plugins/test/login.html",
+    "root/externallib/text!root/plugins/test/theme.css",
+    "root/externallib/text!root/plugins/test/login.html",
     "root/externallib/text!root/plugins/test/program.html"
 ];
 
@@ -28,25 +28,25 @@ define(templates, function (theme, loginForm, program) {
     };
 
     // Inject allways our custom theme.
-    //$("#mobilecssurl").html(theme);
+    $("#mobilecssurl").html(theme);
 
     // Replace the sign-up form with our custom template.
-    //$("#add-site_template").html(loginForm);
+    $("#add-site_template").html(loginForm);
 
     // Automatically use the URL of the moodle moot without further checks.
-    //MM.checkSite = function(e) {
-        //MM.addSite(e);
-    //};
+    MM.checkSite = function(e) {
+        MM.addSite(e);
+    };
 
     // Inject allways our custom theme.
-    //MM.loadCachedRemoteCSS = function(e) {
-     //   $("#mobilecssurl").html(theme);
-   // };
+    MM.loadCachedRemoteCSS = function(e) {
+       $("#mobilecssurl").html(theme);
+    };
 
     // Do not display the manage accounts page.
-    //MM._displayManageAccounts = function() {
-      //  MM._displayAddSite();
-    //};
+    MM._displayManageAccounts = function() {
+        MM._displayAddSite();
+    };
 
     MM.registerPlugin(plugin);
 
